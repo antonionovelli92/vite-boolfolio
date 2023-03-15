046a72432a699446296257106f9dbe4dad6f0832
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-15 18:30:35 +0100

    add projectslist

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import ProjectsList from './components/projects/ProjectsList.vue';
 const apiBaseUrl = 'http://127.0.0.1:8000/api';
 
 export default {
   name: 'App',
-  components: { AppHeader },
+  components: { AppHeader, ProjectsList },
   data: () => ({
     projects: [],
   }),
   methods: {
     fetchProjects() {
       axios.get(apiBaseUrl + '/projects').then((res) => {
-        console.log(res.data);
+        this.projects = res.data;
       })
     }
   },
@@ -24,11 +25,7 @@ export default {
 
 <template>
   <AppHeader />
-  <main class="conteiner">
-
-    <ul>
-
-    </ul>
-
+  <main class="container">
+    <ProjectsList :projects="projects" />
   </main>
 </template>
---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'ProjectsList',
+    props: { projects: Array }
+}
+</script>
+
+<template>
+    <section id="projects-list" class="my-4">
+        <ul v-if="projects.length">
+            <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+        </ul>
+        <h4 v-else class="text-center">Non ci sono progetti</h4>
+    </section>
+</template>
