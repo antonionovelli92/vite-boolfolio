650c38c46f0fa11a0cf0b35c97386715d1752a38
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-15 18:11:46 +0100

    add axios and fetchprojects

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+const apiBaseUrl = 'http://127.0.0.1:8000/api';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { AppHeader },
+  data: () => ({
+    projects: [],
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(apiBaseUrl + '/projects').then((res) => {
+        console.log(res.data);
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
 }
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <main class="conteiner">
+
+    <ul>
+
+    </ul>
+
+  </main>
+</template>
