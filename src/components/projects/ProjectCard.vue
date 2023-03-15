61149f1fd633a65a94c4f520b1f55b649622e719
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-15 18:50:11 +0100

    add postdate

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,7 +1,24 @@
 <script>
 export default {
     name: 'ProjectCard',
-    props: { project: Object }
+    props: { project: Object },
+    computed: {
+        postDate() {
+            const date = new Date(this.project.updated_at);
+            let day = date.getDay();
+            let month = date.getMonth();
+            const year = date.getFullYear();
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+            const seconds = date.getSeconds();
+
+
+            if (day < 10) day = '0' + day;
+            if (month < 10) month = '0' + month;
+
+            return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`
+        }
+    }
 }
 </script>
 
@@ -15,7 +32,7 @@ export default {
         </div>
         <div class="card-footer d-flex justify-content-between">
             <address>By {{ project.author }}</address>
-            <time><small>Pubblicato il</small>{{ project.updated_at }}</time>
+            <time><small>Pubblicato il</small> {{ postDate }}</time>
         </div>
     </div>
 </template>
