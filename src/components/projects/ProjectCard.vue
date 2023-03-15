4f2db5e28e6e09946fd97bcdfdcc82eb559fdd7c
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-15 18:43:08 +0100

    add projectcard

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: { project: Object }
+}
+</script>
+
+<template>
+    <div class="card my-3">
+        <div class="card-header">
+            <h2>{{ project.title }}</h2>
+        </div>
+        <div class="card-body d-flex justify-content-between">
+            <p>{{ project.content }}</p>
+        </div>
+        <div class="card-footer d-flex justify-content-between">
+            <address>By {{ project.author }}</address>
+            <time><small>Pubblicato il</small>{{ project.updated_at }}</time>
+        </div>
+    </div>
+</template>
--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,15 +1,17 @@
 <script>
+import ProjectCard from './ProjectCard.vue';
 export default {
     name: 'ProjectsList',
+    components: { ProjectCard },
     props: { projects: Array }
 }
 </script>
 
 <template>
     <section id="projects-list" class="my-4">
-        <ul v-if="projects.length">
-            <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
-        </ul>
+        <div v-if="projects.length">
+            <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+        </div>
         <h4 v-else class="text-center">Non ci sono progetti</h4>
     </section>
 </template>
