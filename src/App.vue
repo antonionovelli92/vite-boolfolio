6ceecca81fb936193c9694a089f921decb4a41ff
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-16 09:27:29 +0100

    fix '$event'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import AppAlert from './components/AppAlert.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
 const apiBaseUrl = 'http://127.0.0.1:8000/api';
 
 export default {
   name: 'App',
-  components: { AppHeader, ProjectsList },
+  components: { AppHeader, AppAlert, ProjectsList },
   data: () => ({
     // credo una condizione dove isloading parte falso(lo spinner di caaricamento) pwe poin inserirlo prima di una chiamata delle api;
     isLoading: false,
+    hasError: false,
     projects: [],
   }),
   methods: {
@@ -33,6 +35,7 @@ export default {
 <template>
   <AppHeader />
   <main class="container">
+    <AppAlert :is-open="hasError" @close="hasError = false" />
     <AppLoader v-if="isLoading" />
     <ProjectsList v-else :projects="projects" />
   </main>
---
+++ src/components/AppAlert.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppAlert',
+    props: { isOpen: Boolean },
+    emits: ['close']
+}
+</script>
+
+<template>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Errore!</strong> Si è verificato un problema nel recupero dei progetti.
+        <button type="button" class="btn-close" @click="$emit('close')"></button>
+    </div>
+</template>
+
+
+
+
+
+<style scoped lang="scss"></style>
